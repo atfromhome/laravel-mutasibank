4a80acaf66aca19bf2236a8e14b7631e7ee34452
Nuradiyana <me@nooradiana.com>
2024-08-08 11:20:08 +0700

    Initial commit

---
+++ src/SkeletonServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace VendorName\Skeleton;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class SkeletonServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('skeleton')
+            ->hasConfigFile()
+            ->hasMigration('create_package_slug_without_prefix_table');
+    }
+}
